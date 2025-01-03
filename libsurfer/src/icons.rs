1822cdcb76831d24c40e35284c1c619c0653b004
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2025-01-03 21:58:11 +0100

    Improve the search functionality

---
+++ libsurfer/src/icons.rs
@@ -0,0 +1,8 @@
+pub const NEXT_RISING_EDGE: &str = "\u{1337}";
+pub const NEXT_FALLING_EDGE: &str = "\u{1338}";
+pub const PREVIOUS_FALLING_EDGE: &str = "\u{1339}";
+pub const PREVIOUS_RISING_EDGE: &str = "\u{133A}";
+pub const NEXT_TRANSITION: &str = "\u{133B}";
+pub const PREVIOUS_TRANSITION: &str = "\u{133C}";
+pub const BUS: &str = "\u{133D}";
+pub const BIT: &str = "\u{133E}";
