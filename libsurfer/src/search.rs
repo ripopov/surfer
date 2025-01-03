eea81e7fc4d0dd4a909fd2d4c24f3c4bc867203f
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2025-01-03 22:41:08 +0100

    Refactor to use inner search function

--- libsurfer/src/search.rs
+++ libsurfer/src/search.rs
@@ -30,72 +30,94 @@ impl WaveData {
                     .get(vidx)
                     .and_then(|id| self.displayed_items.get(id))
                 {
-                    if let Ok(Some(res)) = self.inner.as_waves().unwrap().query_variable(
-                        &variable.variable_ref,
-                        &cursor.to_biguint().unwrap_or_default(),
-                    ) {
-                        if next {
-                            if let Some(ref time) = res.next {
-                                let stime = time.to_bigint();
-                                if stime.is_some() {
-                                    self.cursor.clone_from(&stime);
-                                }
-                            } else {
-                                // No next transition, go to end
-                                self.cursor = Some(self.num_timestamps().expect(
-                                    "No timestamp count even though waveforms should be loaded",
-                                ));
-                            }
-                        } else if let Some(stime) = res.current.unwrap().0.to_bigint() {
-                            let bigone = BigInt::from(1);
-                            // Check if we are on a transition
-                            if stime == *cursor && *cursor >= bigone {
-                                // If so, subtract cursor position by one
-                                if let Ok(Some(newres)) =
-                                    self.inner.as_waves().unwrap().query_variable(
-                                        &variable.variable_ref,
-                                        &(cursor - bigone).to_biguint().unwrap_or_default(),
-                                    )
-                                {
-                                    if let Some(current) = newres.current {
-                                        if let Some(newstime) = current.0.to_bigint() {
-                                            self.cursor = Some(newstime);
-                                        }
-                                    }
-                                }
-                            } else {
-                                self.cursor = Some(stime);
-                            }
-                        }
-
-                        // if zero edges should be skipped
-                        if transition_type == TransitionType::FromZero {
-                            // check if the next transition is 0, if so and requested, go to
-                            // next positive transition
-                            if let Some(time) = &self.cursor {
-                                let next_value = self.inner.as_waves().unwrap().query_variable(
-                                    &variable.variable_ref,
-                                    &time.to_biguint().unwrap_or_default(),
-                                );
-                                if next_value.is_ok_and(|r| {
-                                    r.is_some_and(|r| {
-                                        r.current.is_some_and(|v| match v.1 {
-                                            VariableValue::BigUint(v) => v == BigUint::from(0u8),
-                                            _ => false,
-                                        })
-                                    })
-                                }) {
-                                    self.set_cursor_at_transition(
-                                        next,
-                                        Some(vidx.into()),
-                                        TransitionType::Any,
-                                    );
-                                }
-                            }
-                        }
+                    if let Some(waves) = self.inner.as_waves() {
+                        let num_timestamps = &self
+                            .num_timestamps()
+                            .expect("No timestamp count even though waveforms should be loaded");
+                        self.cursor = Some(find_transition_time(
+                            next,
+                            transition_type,
+                            waves,
+                            variable,
+                            cursor,
+                            num_timestamps,
+                        ));
                     }
                 }
             }
         }
     }
+}
+
+fn find_transition_time(
+    next: bool,
+    transition_type: TransitionType,
+    waves: &crate::wave_container::WaveContainer,
+    variable: &crate::displayed_item::DisplayedVariable,
+    cursor: &BigInt,
+    num_timestamps: &BigInt,
+) -> BigInt {
+    let mut new_cursor = cursor.clone();
+    if let Ok(Some(res)) = waves.query_variable(
+        &variable.variable_ref,
+        &cursor.to_biguint().unwrap_or_default(),
+    ) {
+        if next {
+            if let Some(ref time) = res.next {
+                let stime = time.to_bigint();
+                if stime.is_some() {
+                    new_cursor.clone_from(&stime.unwrap());
+                }
+            } else {
+                // No next transition, go to end
+                new_cursor.clone_from(num_timestamps);
+            }
+        } else if let Some(stime) = &res.current.unwrap().0.to_bigint() {
+            let bigone = &BigInt::from(1);
+            // Check if we are on a transition
+            if stime == cursor && cursor >= bigone {
+                // If so, subtract cursor position by one
+                if let Ok(Some(newres)) = waves.query_variable(
+                    &variable.variable_ref,
+                    &(cursor - bigone).to_biguint().unwrap_or_default(),
+                ) {
+                    if let Some(current) = newres.current {
+                        if let Some(newstime) = current.0.to_bigint() {
+                            new_cursor.clone_from(&newstime);
+                        }
+                    }
+                }
+            } else {
+                new_cursor.clone_from(stime);
+            }
+        }
+
+        // if zero edges should be skipped
+        if transition_type == TransitionType::FromZero {
+            // check if the next transition is 0, if so and requested, go to
+            // next positive transition
+            let next_value = waves.query_variable(
+                &variable.variable_ref,
+                &new_cursor.to_biguint().unwrap_or_default(),
+            );
+            if next_value.is_ok_and(|r| {
+                r.is_some_and(|r| {
+                    r.current.is_some_and(|v| match v.1 {
+                        VariableValue::BigUint(v) => v == BigUint::from(0u8),
+                        _ => false,
+                    })
+                })
+            }) {
+                new_cursor = find_transition_time(
+                    next,
+                    TransitionType::Any,
+                    waves,
+                    variable,
+                    &new_cursor,
+                    num_timestamps,
+                );
+            }
+        }
+    }
+    new_cursor
 }
