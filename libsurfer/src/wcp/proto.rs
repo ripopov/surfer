01854c696916ce8f1926279a1723353a81b6662c
TheZoq2 <frans.skarman@protonmail.com>
2025-01-09 15:47:26 +0100

    Move wcp documentation into wcp repo and fix some issues

--- libsurfer/src/wcp/proto.rs
+++ libsurfer/src/wcp/proto.rs
@@ -43,10 +43,10 @@ pub struct ItemInfo {
 #[serde(tag = "command")]
 #[allow(non_camel_case_types)]
 pub enum WcpResponse {
-    get_item_list(Vec<String>),
-    get_item_info(Vec<ItemInfo>),
-    add_variables(Vec<DisplayedItemRef>),
-    add_scope(Vec<DisplayedItemRef>),
+    get_item_list{ids: Vec<DisplayedItemRef>},
+    add_variables{ids: Vec<DisplayedItemRef>},
+    add_scope{ids: Vec<DisplayedItemRef>},
+    get_item_info{info: Vec<ItemInfo>},
     ack,
 }
 
@@ -95,60 +95,18 @@ impl WcpSCMessage {
 #[serde(tag = "command")]
 #[allow(non_camel_case_types)]
 pub enum WcpCommand {
-    /// Responds with [WcpResponse::get_item_list] which contains a list of the items
-    /// in the currently loaded waveforms
     get_item_list,
-    /// Responds with [WcpResponse::get_item_info] which contains information about
-    /// each item specified in `ids` in the same order as in the `ids` array.
-    /// Responds with an error if any of the specified IDs are not items in the currently loaded
-    /// waveform.
     get_item_info { ids: Vec<DisplayedItemRef> },
-    /// Changes the color of the specified item to the specified color.
-    /// Responds with [WcpResponse::ack]
-    /// Responds with an error if the `id` does not exist in the currently loaded waveform.
     set_item_color { id: DisplayedItemRef, color: String },
-    /// Adds the specified variables to the view.
-    /// Responds with [WcpResponse::add_variables] which contains a list of the item references
-    /// that can be used to reference the added items later
-    /// Responds with an error if no waveforms are loaded
     add_variables { names: Vec<String> },
-    /// Adds all variables in the specified scope to the view.
-    /// Responds with [WcpResponse::add_variables] which contains a list of the item references
-    /// that can be used to reference the added items later
-    /// Responds with an error if no waveforms are loaded
     add_scope { scope: String },
-    /// Reloads the waveform from disk if this is possible for the current waveform format.
-    /// If it is not possible, this has no effect.
-    /// Responds instantly with [WcpResponse::ack]
-    /// Once the waveforms have been loaded, a separate event is triggered
     reload,
-    /// Moves the viewport to center it on the specified timestamp. Does not affect the zoom
-    /// level.
-    /// Responds with [WcpResponse::ack]
-    set_viewport_to { timestamp: BigInt },
-    /// Removes the specified items from the view.
-    /// Responds with [WcpResponse::ack]
-    /// Does not error if some of the IDs do not exist
     remove_items { ids: Vec<DisplayedItemRef> },
-    /// Sets the specified ID as the _focused_ item.
-    /// Responds with [WcpResponse::ack]
-    /// Responds with an error if no waveforms are loaded or if the item reference
-    /// does not exist
-    // FIXME: What does this mean in the context of the protocol in general, feels kind
-    // of like a Surfer specific thing. Do we have a use case for it
     focus_item { id: DisplayedItemRef },
-    /// Removes all currently displayed items
-    /// Responds with [WcpResponse::ack]
-    clear,
-    /// Loads a waveform from the specified file.
-    /// Responds instantly with [WcpResponse::ack]
-    /// Once the file is loaded, a [WcpEvent::waveform_loaded] is emitted.
-    load { source: String },
-    /// Zooms out fully to fit the whole waveform in the view
-    /// Responds instantly with [WcpResponse::ack]
     zoom_to_fit { viewport_idx: usize },
-    /// Shut down the WCP server.
-    // FIXME: What does this mean? Does it kill the server, the current connection or surfer itself?
+    set_viewport_to { timestamp: BigInt },
+    clear,
+    load { source: String },
     shutdowmn,
 }
 
