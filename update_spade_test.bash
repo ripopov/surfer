amcolex/surfer-fork

Allow translators to translate names into source code
Use variable_name_info in AnyTranslator

update_spade_test.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-SPADE_REV="10e9fdd6d28e2555b7ae92c185116f8a1349f880"
+SPADE_REV="e3615dbb3d3f3b2e0d29a1fa36b47e5d11a24272"
 
 d="$(mktemp -d)"
-pushd $d
+pushd "$d"
 git clone https://gitlab.com/spade-lang/spade
 cd spade
 git checkout -d $SPADE_REV
 cd swim_tests
 swim test pipeline_ready_valid --testcases enabled_stages_behave_normally
 popd
-cp $d/spade/swim_tests/build/state.ron ./examples/spade_state.ron
-cp $d/spade/swim_tests/build/pipeline_ready_valid_enabled_stages_behave_normally/pipeline_ready_valid.vcd ./examples/spade.vcd
+cp "$d/spade/swim_tests/build/state.ron" ./examples/spade_state.ron
+cp "$d/spade/swim_tests/build/pipeline_ready_valid_enabled_stages_behave_normally/pipeline_ready_valid.vcd" ./examples/spade.vcd
 
-rm -rf $d
+rm -rf "$d"

ripopov/surfer

update_spade_test.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-SPADE_REV="03fcfc640df3f52def3e45a3ba2ea2c91a52d7bf"
+SPADE_REV="b7d5dc6c18f1e58d872ede81ff7c8b8c23d41ffe"
 
 d="$(mktemp -d)"
-pushd $d
+pushd "$d"
 git clone https://gitlab.com/spade-lang/spade
 cd spade
 git checkout -d $SPADE_REV
 cd swim_tests
 swim test pipeline_ready_valid --testcases enabled_stages_behave_normally
 popd
-cp $d/spade/swim_tests/build/state.ron ./examples/spade_state.ron
-cp $d/spade/swim_tests/build/pipeline_ready_valid_enabled_stages_behave_normally/pipeline_ready_valid.vcd ./examples/spade.vcd
+cp "$d/spade/swim_tests/build/state.ron" ./examples/spade_state.ron
+cp "$d/spade/swim_tests/build/pipeline_ready_valid_enabled_stages_behave_normally/pipeline_ready_valid.vcd" ./examples/spade.vcd
 
-rm -rf $d
+rm -rf "$d"